abe83406262d369dfd0c8b21488bc83030aa74cf
devLaka <lakshanwarunab@gmail.com>
2021-11-09 14:54:07 +0530

    add routes

--- index.js
+++ index.js
@@ -5,14 +5,31 @@ const express = require("express");
 const app = express();
 
 // Runs for every request that hits the server
-app.use((req, res) => {
-    console.log("A request hits the server");
-    //text response
-    // res.send("This is a text response!");
-    //json response
-    // res.send({ name: "Jhon" });
-    //html response
-    res.send('<h1> This is the html response </h1>');
+// app.use((req, res) => {
+//     console.log("A request hits the server");
+//     //text response
+//     // res.send("This is a text response!");
+//     //json response
+//     // res.send({ name: "Jhon" });
+//     //html response
+//     res.send('<h1> This is the html response </h1>');
+// });
+
+app.get('/', (req, res) => {
+    res.send('Home Page');
+});
+
+app.get('/about-me', (req, res) => {
+    res.send('About Me Page');
+});
+
+app.post('/details', (req, res) => {
+    res.send('Post request');
+});
+
+// Matches all requests
+app.get('*', (req, res) => {
+    res.send('Rounter is undefined');
 });
 
 // Starting the server
